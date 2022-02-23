0a2fc1537dd26fb5f14fe421713e7f70e173b610
VishalPaudel <vishal.paudel@plakhsa.edu.in>
2022-02-23 14:49:11 +0530

    _

--- Arduino/sketch_feb10a/sketch_feb10a.ino
+++ Arduino/sketch_feb10a/sketch_feb10a.ino
@@ -1,9 +1,15 @@
 int Motor1_Pin1 = 4;
 int Motor1_Pin2 = 5;
 
+int Motor2_Pin1 = 2;
+int Motor2_Pin2 = 3;
+
 void setup() {
   // put your setup code here, to run once:
   
+  pinMode(Motor1_Pin1, OUTPUT);
+  pinMode(Motor1_Pin2, OUTPUT);
+
   pinMode(Motor1_Pin1, OUTPUT);
   pinMode(Motor1_Pin2, OUTPUT);
 }
@@ -12,5 +18,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(Motor1_Pin1, HIGH);
   digitalWrite(Motor1_Pin2, LOW);
+  digitalWrite(Motor2_Pin1, HIGH);
+  digitalWrite(Motor2_Pin2, LOW);
   delay(1000);
 }
